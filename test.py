6696073c9aaf676f93508644a3519584e45e8244
KetiLion <1@gmail.com>
2023-04-07 19:45:01 -0700

    сохраняем

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-# Тупо тест, шоб затэздидь )
+# 7.1. Найдите кпопку c текстом "Gold". Попробуйте подобрать как минимум 2 разных XPATH и/или CSS-селекторов
+# http://suninjuly.github.io/xpath_examples
+
+# XPATH
+/html/body/div[2]/button[3]
+//body/div[2]/button[3]
+//div[2]/button[3]
+//button[3][text() ='Gold']
+//* [contains(text(),'Gold')]
+//button [contains(text(),'Gold')]
+//button [contains(text(),'Gol')]
+
+# play for Select
+# https://flukeout.github.io/
+# https://topswagcode.com/xpath/
